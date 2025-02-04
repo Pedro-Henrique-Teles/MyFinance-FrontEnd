7c7e036790c69975be91b5304588f928f9dc7778
pedro-henrique-teles <pedrohenriqueteles732@gmail.com>
2025-02-03 21:25:19 -0300

    Refactor Login component to use Tailwind CSS for styling and simplify form structure

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,69 +1,91 @@
-import React from "react";
-import {
-  Modal,
-  ModalContent,
-  ModalHeader,
-  ModalBody,
-  ModalFooter,
-  Button,
-  useDisclosure,
-} from "@heroui/react";
+import { Button, Input } from "@heroui/react";
 
-function Login() {
-  const {isOpen, onOpen, onClose} = useDisclosure();
-  const [size, setSize] = React.useState("md");
-
-  const sizes = ["xs", "sm", "md", "lg", "xl", "2xl", "3xl", "4xl", "5xl", "full"];
-
-  const handleOpen = (size) => {
-    setSize(size);
-    onOpen();
-  };
-
+export default function Login() {
   return (
     <>
-      <div className="flex flex-wrap gap-3">
-        {sizes.map((size) => (
-          <Button key={size} onPress={() => handleOpen(size)}>
-            Open {size}
-          </Button>
-        ))}
-      </div>
-      <Modal isOpen={isOpen} size={size} onClose={onClose}>
-        <ModalContent>
-          {(onClose) => (
-            <>
-              <ModalHeader className="flex flex-col gap-1">Modal Title</ModalHeader>
-              <ModalBody>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam pulvinar risus non
-                  risus hendrerit venenatis. Pellentesque sit amet hendrerit risus, sed porttitor
-                  quam.
-                </p>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam pulvinar risus non
-                  risus hendrerit venenatis. Pellentesque sit amet hendrerit risus, sed porttitor
-                  quam.
-                </p>
-                <p>
-                  Magna exercitation reprehenderit magna aute tempor cupidatat consequat elit dolor
-                  adipisicing. Mollit dolor eiusmod sunt ex incididunt cillum quis. Velit duis sit
-                  officia eiusmod Lorem aliqua enim laboris do dolor eiusmod.
-                </p>
-              </ModalBody>
-              <ModalFooter>
-                <Button color="danger" variant="light" onPress={onClose}>
-                  Close
+      <section className="flex items-center justify-center bg-gradient-to-r from-bgColorPrimary to-bgColorSecondary relative h-screen w-screen overflow-hidden">
+        <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+          <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+            <img
+              alt="Your Company"
+              src="../img/ImageBackground.png"
+              className="mx-auto h-20 w-auto rounded-full"
+            />
+            <h2 className="mt-10 text-center text-2xl/9 font-bold tracking-tight text-white">
+            Login
+            </h2>
+          </div>
+
+          <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+            <form action="#" method="POST" className="space-y-6">
+              <div>
+                <label
+                  htmlFor="cpf"
+                  className="block text-sm/6 font-medium text-white"
+                >
+                  CPF
+                </label>
+                <div className="mt-2">
+                  <Input
+                    isRequired
+                    id="cpf"
+                    errorMessage="Por favor, preencha o campo CPF"
+                    name="cpf"
+                    placeholder="Digite seu CPF"
+                    type="text"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <div className="flex items-center justify-between">
+                  <label
+                    htmlFor="password"
+                    className="block text-sm/6 font-medium text-white"
+                  >
+                    Senha
+                  </label>
+                  <div className="text-sm">
+                    <a
+                      href="#"
+                      className="font-semibold text-primary hover:text-primaryHover"
+                    >
+                      Esqueceu Sua Senha ?
+                    </a>
+                  </div>
+                </div>
+                <div className="mt-2">
+                  <Input
+                    isRequired
+                    id="cpf"
+                    errorMessage="Por favor, preencha o campo CPF"
+                    name="cpf"
+                    placeholder="Digite seu CPF"
+                    type="text"
+                  />
+                </div>
+              </div>
+
+              <div>
+                <Button
+                  color="primary"
+                  variant="shadow"
+                  size="lg"
+                  className="flex w-full justify-center rounded-md px-3 py-1.5 text-sm/6 font-semibold text-white shadow-xs focus-visible:outline-2 focus-visible:outline-offset-2"
+                >
+                  Login
                 </Button>
-                <Button color="primary" onPress={onClose}>
-                  Action
-                </Button>
-              </ModalFooter>
-            </>
-          )}
-        </ModalContent>
-      </Modal>
+              </div>
+            </form>
+            <p className="mt-10 text-center text-sm/6 text-gray-500">
+            Não tem uma conta ?{' '}
+            <a href="/signin" className="font-semibold text-primary hover:text-primaryHover">
+              Faça o cadastro agora
+            </a>
+          </p>
+          </div>
+        </div>
+      </section>
     </>
   );
 }
-export default Login;
